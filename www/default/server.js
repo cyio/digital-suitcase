93372725abdfe740c7bab9b169c624c3d3fea393
cyio <icaner@qq.com>
2015-11-22 19:07:02 +0800

    update

--- www/default/server.js
+++
@@ -1,5 +0,0 @@
-var connect = require('connect');
-var serve = require('serve-static');
-
-connect().use(serve(__dirname + '/public')).listen(1337);
-console.log('server is listening on port 1337...');
