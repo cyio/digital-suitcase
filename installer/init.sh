1af3a55033a3ad1e944de7196772a48ad3bfe462
cyio <icaner@qq.com>
2016-04-11 18:27:46 +0800

    update

--- installer/init.sh
+++ installer/init.sh
@@ -1,3 +1,4 @@
 apt-get install software-properties-common -y
 apt-get install git curl -y
 apt-get install silversearcher-ag -y
+apt-get install fcitx-table-wbpy -y
--- installer/smart-npm.sh
+++ installer/smart-npm.sh
@@ -1,2 +1,2 @@
-ln -s $(npm root -g)/npm/bin/npm-cli.js $(dirname $(which npm))/npm-original
+sudo ln -s $(npm root -g)/npm/bin/npm-cli.js $(dirname $(which npm))/npm-original
 sudo npm install --global smart-npm --registry=https://registry.npm.taobao.org/
