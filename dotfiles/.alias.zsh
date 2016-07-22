5962487f56e418b308e81c0937373bd10bc55b55
cyio <icaner@qq.com>
2016-07-22 17:02:46 +0800

    auto-commit

--- dotfiles/.alias.zsh
+++ dotfiles/.alias.zsh
@@ -1,9 +1,14 @@
+alias byt='bytewiser'
 alias dt='date --rfc-3339="date"'
 alias v='vim'
 alias z='cd'
 alias hig='history|grep'
 alias open='xdg-open'
 # alias vim="nvim"
+# mosh
+alias spc='mosh oaker@192.168.1.143'
+alias scb='mosh oaker@192.168.1.115'
+alias smb='mosh --ssh="ssh -p 8022" u0_a78@192.168.1.233'
 function gi() { curl -L -ttps://www.gitignore.io/api/$@ ;}
 function lazygit() {
   git add .
