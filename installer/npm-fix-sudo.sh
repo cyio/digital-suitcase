9b42579cb021e0cfd1bb97ec94b12cdad449ad08
cyio <icaner@qq.com>
2016-07-31 02:29:02 -0400

    update

--- installer/npm-fix-sudo.sh
+++ installer/npm-fix-sudo.sh
@@ -1,4 +1,8 @@
- mkdir ~/.npm-packages
- npm config set prefix '~/.npm-packages'
- export PATH=~/.npm-packages/bin:$PATH
- source ~/.profile
+mkdir ~/.npm-packages
+npm config set prefix '~/.npm-packages'
+vim ~/.profile
+# add following line
+export PATH=~/.npm-packages/bin:$PATH
+source ~/.profile
+# check
+npm config get config
